34cbbfa6665dac455648de61258d7ca51e56aea4
VedantRaval3011 <121101897+VedantRaval3011@users.noreply.github.com>
2024-03-12 23:47:51 +0530

    Add files via upload

---
+++ src/views/Primary-data-main-page.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="mainContainer">
+    <div class="top-container">
+      <div class="logo">
+        <img src="../assets/vedas.png" alt="vedas logo" />
+      </div>
+      <div class="user">
+        <img class="user-button" src="../assets/userbg.png" alt="user-image" />
+      </div>
+    </div>
+    <div class="upper-container">
+        <div class="heading">
+        <h1>Primary Data Management</h1>
+        </div>
+        <div class="add-button">
+            <button>Add data</button>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .top-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 0;
+  background: inherit;
+  display: flex;
+  justify-content: space-between;
+}
+
+.logo {
+  margin-right: 20px; /* Add some spacing between the logo and other content */
+  position: fixed;
+}
+
+.logo img {
+  margin-left: 20px;
+}
+
+.user-button {
+  width: 80px;
+  margin: 33px;
+  border-radius: 50%; /* Add rounded corners */
+  box-shadow: 0px 1px 5px rgb(94, 77, 77);
+  position: fixed;
+  right: 0
+}
+
+.form-container-6{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+
+.user-button:hover {
+  box-shadow: 0px 0px 0px rgb(94, 77, 77);
+  cursor: pointer;
+  z-index: 20;
+}
+
+.user-button {
+  --clr-font-main: hsla(0 0% 20% / 100);
+  --btn-bg-1: rgb(65, 145, 249);
+  --btn-bg-2: hsla(194 100% 56% / 1);
+  --btn-bg-color: hsla(360 100% 100% / 1);
+  --radii: 0.5em;
+  cursor: pointer;
+  min-width: 120px;
+  font-size: var(--size, 1rem);
+  font-family: "Segoe UI", system-ui, sans-serif;
+  font-weight: 500;
+  transition: 0.8s;
+  background-size: 280% auto;
+  border: none;
+  width: 50px;
+  border-radius: 50%;
+  color: var(--btn-bg-color);
+  box-shadow: 0px 0px 20px rgba(71, 184, 255, 0.5), 0px 5px 5px -1px rgba(58, 125, 233, 0.25), inset 4px 4px 8px rgba(175, 230, 255, 0.5), inset -4px -4px 8px rgba(19, 95, 216, 0.35);
+}
+
+
+
+</style>
